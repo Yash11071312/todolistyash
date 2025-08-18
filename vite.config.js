53fb01288f7c03fe4c985f07b95ad68c6d338c0c
yash <ykhedekar1107@gmail.com>
2025-08-18 20:31:57 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/todolistyashnew/', // 👈 Repo name goes here
+  plugins: [react()],
 })
